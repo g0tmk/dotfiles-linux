317b41010a5eb2df756678f0b6e215fb6cef430a
g0tmk <brandon.ivy@gmail.com>
2019-05-08 17:46:21 -0700

    fix yeganesh and add todos

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -44,19 +44,19 @@ main = do
     where 
         myKeys = -- Ctrl+Shift+Z: Lock screen with slock
                  [ ((mod4Mask .|. shiftMask, xK_z), spawn "slock")
-                 -- screenshot 1
-                 -- NOTE: hotkey not working on xps9550
+                 -- screenshot 1 NOTE: hotkey not working - check xK_Print
                  , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s &; echo 'Select area' | show_osd_message")
                  -- screenshot 2
                  , ((0, xK_Print), spawn "scrot")
                  -- Mod+b: toggle fullscreen
                  , ((mod4Mask, xK_b     ), sendMessage ToggleStruts)
-                 -- Mod+p: dmenu (w/ custom fonts and stuff)
+                 -- Mod+p: yeganesh (w/ custom fonts and stuff) NOTE: "$()" syntax will execute the output (yeganesh only outputs the binary's name)
                  --, ((mod4Mask, xK_p), spawn "dmenu_run -fn -*-terminus-*-r-*-*-12-*-*-*-*-*-*-* -nb bisque3 -nf grey35 -sb bisque1 -sf grey10")
                  --, ((mod4Mask, xK_p), spawn "dmenu_run -fn 'Terminus::pixelsize=12:antialias=0'")
-                 , ((mod4Mask, xK_p), spawn "~/bin/yeganesh -x -- -fn 'Terminus::pixelsize=12:antialias=0'")
+                 , ((mod4Mask, xK_p), spawn "$(~/bin/yeganesh -x -- -fn 'Terminus::pixelsize=12:antialias=0')")
                  -- Multimedia keys
                  -- XF86MonBrightnessUp
+                 -- NOTE: x220 seems to do this automatically so we need a way to disable it there; for now, I did not add brightness.py to the sudoers file... its a workaround but should be fixed asap
                  , ((0, 0x1008ff02), spawn "sudo ~/bin/brightness.py increase 10 | show_osd_message")
                  -- XF86MonBrightnessDown
                  , ((0, 0x1008ff03), spawn "sudo ~/bin/brightness.py decrease 10 | show_osd_message")

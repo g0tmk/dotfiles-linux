28cde3d3f598d1570edf3dcb8971454fef9d7a91
g0tmk <brandon.ivy@gmail.com>
2019-05-15 13:48:50 -0700

    add scripts used to configure resolution and external monitors on x220. needs to be made generic for other sytems

---
+++ bin/bin/monitor-dual-1440x900-rotated.sh
@@ -0,0 +1,27 @@
+# configure displays for a dual monitor setup with a rotated 1440x900 secondary
+
+xrandr --output HDMI-1 --auto --rotate right --left-of LVDS-1
+sleep 1
+
+# TODO: change stalonetray config if necessary
+#killall stalonetray
+#stalonetray &
+#sleep 1
+
+#ln -sf ~/.xmobarrc-dual-1440x900-rotated ~/.xmobarrc
+xmonad --restart
+sleep 1
+
+#killall synergyc
+#killall synergy  # maybe don't need this
+#~/start-synergy.sh
+#sleep 1
+
+# reload wallpaper
+~/.fehbg
+sleep 1
+
+# todo: figure out why this needs to be run twice
+xmonad --restart
+
+

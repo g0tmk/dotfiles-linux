6f7ab6e60b5f5d86e4aed18132f26b30c479108d
g0tmk <brandon.ivy@gmail.com>
2021-09-19 19:21:15 -0700

    add some old xrandr scripts from bx220 that i'll probably remove later

---
+++ bin/bin/monitor-single_bx220u.sh
@@ -0,0 +1,29 @@
+# configure displays for the single built-in display (1366x768
+
+xrandr --output HDMI-1 --off
+
+sleep 1
+
+# TODO: change stalonetray config if necessary
+killall stalonetray
+stalonetray &
+
+sleep 1
+
+ln -sf ~/.xmobarrc-single ~/.xmobarrc
+xmonad --restart
+
+sleep 1
+
+killall synergyc
+killall synergy  # maybe don't need this
+~/start-synergy.sh
+
+sleep 1
+
+# reload wallpaper
+~/.fehbg
+
+
+# todo: figure out why this needs to be run twice
+xmonad --restart
